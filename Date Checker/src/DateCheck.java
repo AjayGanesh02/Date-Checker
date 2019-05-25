911f275af97cdb452ea5d0f495d7192cae0c2976
AjayGanesh02 <cooltechajay11@gmail.com>
2019-05-25 15:36:13 -0400

    Project started
    The project has been created and some basic setup has been done

---
+++ Date Checker/src/DateCheck.java
@@ -0,0 +1,44 @@
+import java.util.Calendar;
+
+public class DateCheck {
+	
+	
+	
+	public String Check() {
+		String result = "";
+		Calendar cal = Calendar.getInstance();  
+		
+		for (int yearC = 2002; yearC < 2100; yearC++) {   //A year counter is used in a for loop to check each year, starting from my birth in 2002
+			cal.set(Calendar.YEAR, yearC);
+			if () {
+				
+			}
+			
+			
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/* All occurrences of my birthday on Mother's day, copied from my uncle's email:
+Ajay's birthday on Mother's Day in: 12 May, 2002
+Ajay's birthday on Mother's Day in: 12 May, 2013
+Ajay's birthday on Mother's Day in: 12 May, 2019
+Ajay's birthday on Mother's Day in: 12 May, 2024
+Ajay's birthday on Mother's Day in: 12 May, 2030
+Ajay's birthday on Mother's Day in: 12 May, 2041
+Ajay's birthday on Mother's Day in: 12 May, 2047
+Ajay's birthday on Mother's Day in: 12 May, 2052
+Ajay's birthday on Mother's Day in: 12 May, 2058
+Ajay's birthday on Mother's Day in: 12 May, 2069
+Ajay's birthday on Mother's Day in: 12 May, 2075
+Ajay's birthday on Mother's Day in: 12 May, 2080
+Ajay's birthday on Mother's Day in: 12 May, 2086
+Ajay's birthday on Mother's Day in: 12 May, 2097
+*/
